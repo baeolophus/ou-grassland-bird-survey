37921832a58134ad1f01b6d872f11dcba42e3b02
baeolophus <curryclairem@gmail.com>
2016-09-27 16:33:44 -0500

    added and reorganized list of issues with text files.  data manipulation file started in earnest.

--- data_manipulation.R
+++ data_manipulation.R
@@ -1,2 +1,32 @@
-#testing the data
-#for line endings
+#libraries used in this file.
+library(dplyr)
+
+#import data from csv files.
+######
+#point counts
+
+#sightings
+pointcount.data<-read.csv()
+#metadata
+pointcount.metadata<-read.csv()
+#merge the files so every row has all metadata attached.
+pointcount.complete<-left_join(pointcount.data,
+                               pointcount.metadata,
+                               by)
+
+
+#transects
+  
+#sightings
+transect.data<-read.csv()
+#metadata
+transect.metadata<-read.csv()
+#merge the files so every row has all metadata attached.
+transect.complete<-left_join(transect.data,
+                             transect.metadata,
+                             by)
+######
+
+#
+#generate absences for each species 
+#these are needed for species distribution models
