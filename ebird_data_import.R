dd03a2dae7322c717670ac2191051c3f2a9cdb8d
baeolophus <curryclairem@gmail.com>
2016-11-15 12:56:31 -0600

    successfully used library ff and ffbase to load giant csv files and then subset to reasonable sized csv, write to harddrive where I will move over to dropbox for regular use/analysis.  Done for 2013 and 2014, unclear if I will use other years as yet, but code is in format that I can probably make a function pretty easily (just sub year).

--- ebird_data_import.R
+++ ebird_data_import.R
@@ -8,23 +8,66 @@ library(ff)
 library(ffbase)
 
 checklist2014<-read.csv.ffdf(file=paste(year, "/", "checklists.csv", sep=""),
-                                        colClasses=NA, 
-                             transFUN=function(x){x[,20:953] <- as.factor(x[,20:953]); x})
+                                        colClasses=c(
+                                          rep('factor', 2),
+                                          rep('numeric', 6),
+                                          rep('factor', 4),
+                                          rep('numeric', 3),
+                                          rep('factor', 1),
+                                          rep('numeric', 1),
+                                          rep('factor', 2),
+                                          rep('factor', 933)),
+                             na.strings="?")
+
 
 #https://rpubs.com/msundar/large_data_analysis
 #http://stackoverflow.com/questions/6616020/problem-with-specifying-colclasses-in-read-csv-in-r
 #http://www.bnosac.be/index.php/blog/22-if-you-are-into-large-data-and-work-a-lot-package-ff
 #http://stackoverflow.com/questions/13186077/work-in-r-with-very-large-data-set
+#http://stats.stackexchange.com/questions/1471/is-it-possible-to-directly-read-csv-columns-as-categorical-data
+#studyarea.extent<-extent(-103,-96,
+#                         33,38) 
+studyarea.ebird<-subset(checklist2014,
+                        LATITUDE>=33 & LATITUDE<=38 &
+                          LONGITUDE >=-103 & LONGITUDE <=-96)
 
-coordinates(checklist2014)<-c("DECIMAL_LONGITUDE",
-                      "DECIMAL_LATITUDE")
+write.csv.ffdf(studyarea.ebird,
+               file="studyarea_ebird2014.csv")
+
+studyarea.ebird.df<-as.data.frame(studyarea.ebird)
+
+plot(studyarea.ebird.df$LONGITUDE,
+     studyarea.ebird.df$LATITUDE)
 
 
-studyarea.ebird<-crop(checklist2014,
-     studyarea.extent)
 
-plot(studyarea.ebird,
+year2013<-2013
+
+
+checklist2013<-read.csv.ffdf(file=paste(year2013, "/", "checklists.csv", sep=""),
+                             colClasses=c(
+                               rep('factor', 2),
+                               rep('numeric', 6),
+                               rep('factor', 4),
+                               rep('numeric', 3),
+                               rep('factor', 1),
+                               rep('numeric', 1),
+                               rep('factor', 2),
+                               rep('factor', 933)),
+                             na.strings="?")
+
+studyarea.ebird2013<-subset(checklist2013,
+                        LATITUDE>=33 & LATITUDE<=38 &
+                          LONGITUDE >=-103 & LONGITUDE <=-96)
+
+write.csv.ffdf(studyarea.ebird2013,
+               file="studyarea_ebird2013.csv")
+
+studyarea.ebird2013.df<-as.data.frame(studyarea.ebird2013)
+
+plot(studyarea.ebird2013.df$LONGITUDE,
+     studyarea.ebird2013.df$LATITUDE,
+     pch="+",
      add=TRUE)
 
-write.csv(studyarea.ebird,
-          "studyarea_ebird_data.csv")
+
