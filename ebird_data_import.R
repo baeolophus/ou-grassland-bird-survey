407a979d035b6c26d01cf5e4ea9fdd64bc73ddb3
baeolophus <curryclairem@gmail.com>
2016-11-14 20:42:45 -0600

    regular read.csv crashes because insufficient memory.  rewriting with library ff/ffbase.

--- ebird_data_import.R
+++ ebird_data_import.R
@@ -1,15 +1,26 @@
-setwd("E:\\Documents\\college\\OU-postdoc\\research\\grassland_bird_surveys\\ebird_data\\")
+setwd("E:/Documents/college/OU-postdoc/research/grassland_bird_surveys/ebird_data/")
 
-ebird<-read.csv(file="srd_point_data_3km_us48_v2014.csv")
+#ebird.extradata<-read.csv(file="srd_point_data_3km_us48_v2014.csv")
 
 year<-2014
-checklist2014<-read.csv(file=paste(year, "/", "checklists.csv", sep=""))
 
-coordinates(ebird)<-c("DECIMAL_LONGITUDE",
+library(ff)
+library(ffbase)
+
+checklist2014<-read.csv.ffdf(file=paste(year, "/", "checklists.csv", sep=""),
+                                        colClasses=NA, 
+                             transFUN=function(x){x[,20:953] <- as.factor(x[,20:953]); x})
+
+#https://rpubs.com/msundar/large_data_analysis
+#http://stackoverflow.com/questions/6616020/problem-with-specifying-colclasses-in-read-csv-in-r
+#http://www.bnosac.be/index.php/blog/22-if-you-are-into-large-data-and-work-a-lot-package-ff
+#http://stackoverflow.com/questions/13186077/work-in-r-with-very-large-data-set
+
+coordinates(checklist2014)<-c("DECIMAL_LONGITUDE",
                       "DECIMAL_LATITUDE")
 
 
-studyarea.ebird<-crop(ebird,
+studyarea.ebird<-crop(checklist2014,
      studyarea.extent)
 
 plot(studyarea.ebird,
