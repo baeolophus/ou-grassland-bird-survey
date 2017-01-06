81b403c6fdfcd14507b203bc6ac3d299d80c19b2
baeolophus <curryclairem@gmail.com>
2017-01-05 18:07:13 -0600

    Getting predictor data into R.

---
+++ predictors_import.R
@@ -0,0 +1,5 @@
+#Bringing in predictors.
+
+#NASS- raster, each year has its own
+#https://www.nass.usda.gov/Research_and_Science/Cropland/SARS1a.php
+#Includes switchgrass code, so even if switchgrass not found in OK could do analysis nationwide??
