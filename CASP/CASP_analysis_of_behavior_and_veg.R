3128132253d5a5a9a2a50809e78d973d4956754b
baeolophus <curryclairem@gmail.com>
2017-02-07 14:34:01 -0600

    Starting code to import CASP files, considering what analyses should be.

---
+++ CASP/CASP_analysis_of_behavior_and_veg.R
@@ -0,0 +1,13 @@
+#Cassin's Sparrows
+
+CASP_behavior <- read.csv(file = "CASP/20170207_CASP_playback_responses_combined_longform.csv")
+CASP_veg_daub <- read.csv(file = "CASP/combined_vegetation_percent_cover_CASP.csv")
+CASP_veg_sp   <- read.csv(file = "CASP/combined_vegetation_surveys_CASP.csv")
+
+#Distance between grid points
+
+#Strongest reaction by veg where present
+#Highest reaction on scale
+#Closest approach
+
+#Presence/absence by veg
