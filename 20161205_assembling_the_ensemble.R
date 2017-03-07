f52fc287815cbad949292d3de420a2e6c447cf1e
baeolophus <curryclairem@gmail.com>
2017-03-07 17:40:14 -0600

    Organizing libraries.  Tidying code elsewhere.

--- 20161205_assembling_the_ensemble.R
+++ 20161205_assembling_the_ensemble.R
@@ -1,14 +1,16 @@
 #libraries needed
-library(raster)
-library(sp)
-library(rgdal)
-library(dplyr)
-library(caret)
-library(rpart)
-library(randomForest)
-library(ROCR)
 library(beepr)
+library(caret)
+library(dplyr)
+library(microbenchmark)
+library(randomForest)
+library(raster)
+library(rgdal)
+library(ROCR)
+library(rpart)
+library(sp)
 
+#create temporary raster files on large drive because they occupy 10-30 GB
 rasterOptions()$tmpdir
 rasterOptions(tmpdir="E:/Documents/R/temp")
 
@@ -249,9 +251,7 @@ endCluster()
 
 overlaps.lm <- lm (overlapsperpixel ~ set,
                    data = overlaps)
-library(car)
-Anova(overlaps.lm,
-      type = 3)
+anova(overlaps.lm)
 
 #########################
 #Now, function for subsetting and running the test on each subset.
@@ -292,7 +292,7 @@ spatial.support.set<-function(whichrandombox,
 
 ###########################
 #test with single dataframes
-spatial.support.set<-latlong.predictors.DICK.spatial[polys.df[1,],]
+spatial.support.set<-latlong.predictors.DICK.spatial[polys.medium.df[1,],]
 #I think there should be a line that turns it back into regular data?
 sample.size.good<-ifelse(length(spatial.support.set)>25, 1, 0)
 #need to have the minimum data requirement in here too.
@@ -300,15 +300,14 @@ support.set.data <- as.data.frame(spatial.support.set)
 support.set.data$Longitude <- NULL
 support.set.data$Latitude <- NULL
 
-library(randomForest)
-library(microbenchmark)
+
 tree.test <- randomForest(presence ~ ., 
                           data = support.set.data,
                           ntree = 50) #This allows all other random forest arguments to be set at the spatial.support.set function level.
 #ca 1 sec run
 
 support.set<-crop(predictors_stack,
-                  extent(polys.df[1,]))
+                  extent(polys.medium.df[1,]))
 
 beginCluster()
 microbenchmark(preds_rf<- clusterR(support.set, raster::predict, args = list(model = tree.test)), times = 1)
@@ -326,34 +325,6 @@ beep()
 ############################
 #assemble the support set lists into a model prediction surface.
 #Mosaic function
-#Get the weights out, which is the support set sample size at each pixel
-weights<-lapply(list.test,
-                "[",
-                2)
-weights<-as.vector(unlist(weights))
-#The weights are 1 (have enough samples) and 0 (do not have enough samples, do not use)
-#For use in weighted.mean below.  It is a vector of weights per layer.  Since values are 1 if enough sample
-#then all are weighted equally if they are used.  If 0 (not enough sample size), then 0 weighted, not used.
-
-#remove one level of list on the remaining support sets
-predictions.support.sets<-unlist(lapply(list.test,
-                                        "[",
-                                        1))
-
-#Stack all the support sets (this will work because they have been extended with NA in non-support-set regions)
-predictions.support.sets.stacked<-raster::stack(predictions.support.sets)
-
-
-#Run the ensemble by using weighted mean.  The weight is by number of support sets.
-ensemble.weighted.mosaic<-do.call(raster::weighted.mean,
-                                  list(predictions.support.sets.stacked,
-                                       weights,
-                                       TRUE))
-
-#Plot the mosaic.
-plot(ensemble.weighted.mosaic)
-
-#function version.
 ensemble.function <- function (list.of.rasters) {
   weights<-lapply(list.of.rasters,
                   "[",
@@ -509,3 +480,34 @@ perf_ROC <- performance(pred,
                      "fpr") 
 plot(perf_ROC, main="ROC plot")
 text(0.5,0.5,paste("AUC = ",format(AUC, digits=5, scientific=FALSE)))
+
+#############################
+#############################
+#############################
+#ensemble mosaic non-function version in case needed later
+#Get the weights out, which is the support set sample size at each pixel
+weights<-lapply(list.test,
+                "[",
+                2)
+weights<-as.vector(unlist(weights))
+#The weights are 1 (have enough samples) and 0 (do not have enough samples, do not use)
+#For use in weighted.mean below.  It is a vector of weights per layer.  Since values are 1 if enough sample
+#then all are weighted equally if they are used.  If 0 (not enough sample size), then 0 weighted, not used.
+
+#remove one level of list on the remaining support sets
+predictions.support.sets<-unlist(lapply(list.test,
+                                        "[",
+                                        1))
+
+#Stack all the support sets (this will work because they have been extended with NA in non-support-set regions)
+predictions.support.sets.stacked<-raster::stack(predictions.support.sets)
+
+
+#Run the ensemble by using weighted mean.  The weight is by number of support sets.
+ensemble.weighted.mosaic<-do.call(raster::weighted.mean,
+                                  list(predictions.support.sets.stacked,
+                                       weights,
+                                       TRUE))
+
+#Plot the mosaic.
+plot(ensemble.weighted.mosaic)
