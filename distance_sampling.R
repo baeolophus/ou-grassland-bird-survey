d00b3e8b31b066d3a8353188c9f39e35bcd9a73b
baeolophus <curryclairem@gmail.com>
2017-03-16 10:43:46 -0500

    Got rid of sample code from help.

--- distance_sampling.R
+++ distance_sampling.R
@@ -4,18 +4,6 @@ library(rgeos)
 library(rgdal)
 library(sp)
 
-data(book.tee.data)
-tee.data<-book.tee.data$book.tee.dataframe[book.tee.data$book.tee.dataframe$observer==1,]
-ds.model <- ds(tee.data,4)
-summary(ds.model)
-plot(ds.model)
-
-ds.minke<-ds(minke, truncation="10%")
-plot(ds.minke)
-summary(ds.minke)
-
-ds.gof(ds.minke)
-
 #Transect analysis
 #import data
 source(file = "source_transect_data.R")
