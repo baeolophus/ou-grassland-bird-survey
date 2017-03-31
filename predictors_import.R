e584607e5f47b739eb022d87c9db8e54d61665f0
baeolophus <curryclairem@gmail.com>
2017-03-31 15:35:38 -0500

    rasterizing code.  doublechecked both rasters in qgis and they look good- NA outside state and the correct values in state.

--- predictors_import.R
+++ predictors_import.R
@@ -208,12 +208,10 @@ writeRaster(utm.bioclim,
             overwrite = TRUE)
 ##################################
 #Create mean time and length rasters to match other ok-census cropped files.
-#Also get sample sizes here.
 get.mean.efforts.for.rasters <- read.csv(file = "oklahomadatasetforsdm_naomit_utm.csv")
 
 narrowed <- get.mean.efforts.for.rasters %>% group_by(SAMPLING_EVENT_ID) %>%
   distinct(SAMPLING_EVENT_ID, .keep_all = TRUE)
-#length of narrowed is sample size for training dataset.
 
 numbers <- ungroup(narrowed) %>% summarize (meantime = mean(effort_time, na.rm = TRUE),
                                  meanlength = mean(effort_length, na.rm = TRUE))
@@ -221,8 +219,24 @@ numbers <- ungroup(narrowed) %>% summarize (meantime = mean(effort_time, na.rm =
 grs80.14<-CRS("+proj=utm +zone=14 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs")
 
 #Create masking raster y out of Oklahoma.
-blank_oklahoma_census<-raster("E:/Documents/college/OU-postdoc/research/grassland_bird_surveys/GIS_layers_original/land_use_land_cover_NLCD_ok_3276698_02/land_use_land_cover/nlcd_ok_utm14.tif")
+blank_oklahoma_census<-raster("E:/Documents/college/OU-postdoc/research/grassland_bird_surveys/ougrassland/gis_layers_processed/nlcd_processing/nlcd_cropped_to_ok_census/nlcd_ok_utm14_okmask.tif")
 
+state<-readOGR(dsn="E:/Documents/college/OU-postdoc/research/grassland_bird_surveys/ougrassland/gis_layers_processed",
+               layer="ok_state_vector_smallest_pdf_3158")
+
+state<-spTransform(x = state,
+                   CRS(as.character("+proj=utm +zone=14 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs"))
+)
+
+effort.length.ok.census <- rasterize(x = state,
+                                     y = blank_oklahoma_census,
+                                     field = numbers$meanlength,
+                                     background = NA)
+
+effort.time.ok.census <- rasterize(x = state,
+                                     y = blank_oklahoma_census,
+                                     field = numbers$meantime,
+                                     background = NA)
 
 writeRaster(effort.length.ok.census,
             filename = "effort_length_ok_census_mask.tif",
