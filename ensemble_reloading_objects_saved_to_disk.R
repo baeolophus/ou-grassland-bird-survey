a5267f3e4d52b209985ea33b56627c3d8cfbaca2
baeolophus <curryclairem@gmail.com>
2017-04-11 10:18:12 -0500

    Importing files to examine results.

--- ensemble_reloading_objects_saved_to_disk.R
+++ ensemble_reloading_objects_saved_to_disk.R
@@ -1,12 +1,12 @@
-wdrasters_list <- list.files("E:/Documents/college/OU-postdoc/research/grassland_bird_surveys/ougrassland/ensemble_results",
+wdrasters_list <- list.files("E:/Documents/college/OU-postdoc/research/grassland_bird_surveys/ougrassland/ensemble_results/HOLA_test",
                              pattern = "tif$",
                              full.names = FALSE)
-wdrasters_files <- paste0("E:/Documents/college/OU-postdoc/research/grassland_bird_surveys/ougrassland/ensemble_results",
+wdrasters_files <- paste0("E:/Documents/college/OU-postdoc/research/grassland_bird_surveys/ougrassland/ensemble_results/HOLA_test",
                           "/",
                           wdrasters_list)
 
 for(i in wdrasters_files) { assign(unlist(strsplit(i,
-                                                   "[/]"))[9], #splits filenames at / and and . to eliminate folder name and file type.
+                                                   "[/]"))[10], #splits filenames at / and and . to eliminate folder name and file type.
                                    raster(i)) } 
 
 plot(HOLA_ensemble.weighted.mosaicsupport.large.list.tif)
