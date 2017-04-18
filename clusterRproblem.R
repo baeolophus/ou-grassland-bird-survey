2e3ea91e790885c104186bf6bb0ce88ca080ddd0
baeolophus <curryclairem@gmail.com>
2017-04-18 16:34:22 -0500

    Attempting a reproducible example of getting clusterR to work to try on stackoverflow or at library.

---
+++ clusterRproblem.R
@@ -0,0 +1,23 @@
+library(randomForest)
+library(raster)
+
+iris.rf <- randomForest(Species ~ ., data=iris, replace = FALSE)
+
+
+Sepal.Length <- raster()
+Sepal.Length[] <- seq(from = min(iris$Sepal.Length), to = max(iris$Sepal.Length), length.out = ncell(Sepal.Length))
+Sepal.Width <- raster()
+Sepal.Width[] <- seq(from = min(iris$Sepal.Width), to = max(iris$Sepal.Width), length.out = ncell(Sepal.Width))
+Petal.Length <- raster()
+Petal.Length[] <- seq(from = min(iris$Petal.Length), to = max(iris$Petal.Length), length.out = ncell(Petal.Length))
+Petal.Width <- raster()
+Petal.Width[] <- seq(from = min(iris$Petal.Width), to = max(iris$Petal.Width), length.out = ncell(Petal.Width))
+
+beginCluster()
+r <- stack(Sepal.Length,
+           Sepal.Width,
+           Petal.Length,
+           Petal.Width)
+predicted.r <- raster::predict(object = r,
+                               model = iris.rf,
+                               progress = "text")
