efa0e1d78528229642df4e02790ec513d927a00c
baeolophus <curryclairem@gmail.com>
2017-04-20 18:22:12 -0500

    Made time of day raster and bioclim future rasters.

--- predictors_import.R
+++ predictors_import.R
@@ -207,6 +207,42 @@ writeRaster(utm.bioclim,
             bylayer = TRUE,
             overwrite = TRUE)
 ##################################
+#process the future bioclim layers the same way as bioclim (though use the ok_mask_resample to make sure I don't need to crop again later)
+#import bioclim layers
+future_list <- list.files(path = "E:/Documents/college/OU-postdoc/research/grassland_bird_surveys/GIS_layers_original/he45bi50",
+                          pattern = "tif$",
+                          full.names = TRUE)
+
+for(i in future_list) { assign(unlist(strsplit(i,
+                                               "[./]"))[9], #splits filenames at / and and . to eliminate folder name and file type.
+                               raster(i)) } 
+
+future <- as.list(ls()[sapply(ls(), function(x) class(get(x))) == 'RasterLayer'])
+future_stack <- stack (lapply(future, get))
+crs(future_stack) <- "+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0"  #http://www.worldclim.org/format
+
+studyarea.extent.latlong<-extent(-103,-94,
+                                 33,38) # define the extent for latlong to get a smaller file
+studyarea.future_stack<-crop(future_stack,
+                        studyarea.extent.latlong)
+
+#show they are in different CRS with different extents
+okcensus <- raster("E:/Documents/college/OU-postdoc/research/grassland_bird_surveys/ougrassland/gis_layers_processed/resample_census_utm_30m.tif")
+extent(okcensus)
+extent(studyarea.future_stack)
+
+#project to the smaller extent and the crs of popdensity_census_raster (Which was made with NLCD)
+utm.future <- projectRaster(from = studyarea.future_stack, 
+                             to = okcensus)
+
+#write the new file to smaller files that I can import later  without re-processing
+writeRaster(utm.future,
+            filename = names(utm.future),
+            format = "GTiff",
+            bylayer = TRUE,
+            overwrite = TRUE)
+
+##################################
 #Create mean time and length rasters to match other ok-census cropped files.
 get.mean.efforts.for.rasters <- read.csv(file = "oklahomadatasetforsdm_naomit_utm.csv")
 
@@ -248,4 +284,13 @@ writeRaster(effort.time.ok.census,
             format="GTiff",
             overwrite = TRUE)
 
+time_of_day <- rasterize(x = state,
+                                   y = blank_oklahoma_census,
+                                   field = 7,
+                                   background = NA)
+
+writeRaster(time_of_day,
+            filename = "time_of_day.tif",
+            format="GTiff",
+            overwrite = TRUE)
 
