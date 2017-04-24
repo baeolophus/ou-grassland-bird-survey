9c8ce66d9b3f5c6ad9b68b3b7463a68dde13fe9e
baeolophus <curryclairem@gmail.com>
2017-04-24 14:44:18 -0500

    future bioclim code

--- predictors_import.R
+++ predictors_import.R
@@ -233,11 +233,46 @@ extent(studyarea.future_stack)
 
 #project to the smaller extent and the crs of popdensity_census_raster (Which was made with NLCD)
 utm.future <- projectRaster(from = studyarea.future_stack, 
-                             to = okcensus)
+                             to = okcensus,
+                            filename = names(studyarea.future_stack),
+                            format = "GTiff",
+                            bylayer = TRUE,
+                            overwrite = TRUE)
+
+#Now upload and clip/mask them to ok state polygon.
+state<-readOGR(dsn="E:/Documents/college/OU-postdoc/research/grassland_bird_surveys/ougrassland/gis_layers_processed",
+               layer="ok_state_vector_smallest_pdf_3158")
+state<-spTransform(x = state,
+                   CRS(as.character("+proj=utm +zone=14 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs"))
+)
+
+future_OK <- mask(utm.future,
+                  state,
+                  filename = names(future_OK),
+                  format = "GTiff",
+                  bylayer = TRUE,
+                  overwrite = TRUE)
 
 #write the new file to smaller files that I can import later  without re-processing
-writeRaster(utm.future,
-            filename = names(utm.future),
+writeRaster(future_OK)
+
+
+
+future_crop_list <- list.files(path = "E:/Documents/college/OU-postdoc/research/grassland_bird_surveys/ougrassland/gis_layers_processed/he45bi50",
+                          pattern = "tif$",
+                          full.names = TRUE)
+
+for(i in future_crop_list) { assign(unlist(strsplit(i,
+                                               "[./]"))[10], #splits filenames at / and and . to eliminate folder name and file type.
+                               raster(i)) } 
+
+future_crop <- as.list(ls()[sapply(ls(), function(x) class(get(x))) == 'RasterLayer'])
+future_crop_stack <- stack (lapply(future_crop, get))
+
+
+#write the new file to smaller files that I can import later  without re-processing
+writeRaster(future_OK,
+            filename = names(future_OK),
             format = "GTiff",
             bylayer = TRUE,
             overwrite = TRUE)
