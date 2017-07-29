0bd2113e9d056c3c5eb1e6c65d6fd8dca34c0e17
baeolophus <curryclairem@gmail.com>
2017-07-28 19:26:47 -0500

    function for bringing in evals. separate function for conducting anova and extracting summary.

--- speciesspecific_comparisons.R
+++ speciesspecific_comparisons.R
@@ -2,95 +2,116 @@
 
 setwd("E:/Documents/college/OU-postdoc/research/grassland_bird_surveys/ougrassland/ensemble_results/Current")
 
-SPECIES <- "NOBO"
-SPECIES <- "UPSA"
-SPECIES <- "HOLA"
-SPECIES <- "CASP"
-SPECIES <- "FISP"
-SPECIES <- "LASP"
-SPECIES <- "GRSP"
-SPECIES <- "DICK"
-SPECIES <- "EAME"
-SPECIES <- "WEME"
-SPECIES <- "BHCO"
+p.adjust
+#list of species
+specieslist <- c("NOBO",
+                 "UPSA",
+                 "HOLA",
+                 "CASP",
+                 "FISP",
+                 "LASP",
+                 "GRSP",
+                 "DICK",
+                 "EAME",
+                 "WEME",
+                 "BHCO")
+#evals for all species
 
-
-#loading Rdata gets the rest!
-load(paste0("~/college/OU-postdoc/research/grassland_bird_surveys/ougrassland/ensemble_results/Current/",
-            SPECIES,
-            "/",
-            SPECIES,
-            "_rdata.RData"))
-
-evalresults <- readRDS(file.path(
-  SPECIES,
-  paste0(
+eval.function <- function(SPECIES) {
+  evalresults <- readRDS(file.path(
     SPECIES,
-    "_products_evaluation_results"))
-)
-#contains AUC/RMSE for all.
-eval.df <- data.frame(evalresults)
+    paste0(
+      SPECIES,
+      "_products_evaluation_results"))
+  )
+  #contains AUC/RMSE for all.
+  eval.df <- data.frame(evalresults)
+  
+  listerrornames <- c("statewide.sampling.rmse.sameyear",
+                      "statewide.sampling.auc.sameyear",
+                      "small.sampling.rmse.sameyear",
+                      "small.sampling.auc.sameyear",
+                      "medium.sampling.rmse.sameyear",
+                      "medium.sampling.auc.sameyear",
+                      "large.sampling.rmse.sameyear",
+                      "large.sampling.auc.sameyear",
+                      "statewide.sampling.rmse.diffyear",
+                      "statewide.sampling.auc.diffyear",
+                      "small.sampling.rmse.diffyear",
+                      "small.sampling.auc.diffyear",
+                      "medium.sampling.rmse.diffyear",
+                      "medium.sampling.auc.diffyear",
+                      "large.sampling.rmse.diffyear",
+                      "large.sampling.auc.diffyear")
+  colnames(eval.df) <- listerrornames
+  
+  eval.df.sep <- gather_(eval.df,
+                         key_col = "errordescriptor",
+                         value_col = "errornum",
+                         gather_cols = listerrornames) %>%
+    separate_(col = "errordescriptor",
+              into = c("scale",
+                       "sampling",
+                       "errortype",
+                       "year"))
+  
+  eval.df.sep$scale <- factor(eval.df.sep$scale,
+                              levels = c("statewide",
+                                         "large",
+                                         "medium",
+                                         "small"))
+  
+  eval.df.sep$species <- SPECIES
+  return(eval.df.sep)
+}
 
-listerrornames <- c("statewide.sampling.rmse.sameyear",
-                    "statewide.sampling.auc.sameyear",
-                    "small.sampling.rmse.sameyear",
-                    "small.sampling.auc.sameyear",
-                    "medium.sampling.rmse.sameyear",
-                    "medium.sampling.auc.sameyear",
-                    "large.sampling.rmse.sameyear",
-                    "large.sampling.auc.sameyear",
-                    "statewide.sampling.rmse.diffyear",
-                    "statewide.sampling.auc.diffyear",
-                    "small.sampling.rmse.diffyear",
-                    "small.sampling.auc.diffyear",
-                    "medium.sampling.rmse.diffyear",
-                    "medium.sampling.auc.diffyear",
-                    "large.sampling.rmse.diffyear",
-                    "large.sampling.auc.diffyear")
-colnames(eval.df) <- listerrornames
-
-eval.df.sep <- gather_(eval.df,
-                       key_col = "errordescriptor",
-                       value_col = "errornum",
-                       gather_cols = listerrornames) %>%
-  separate_(col = "errordescriptor",
-            into = c("scale",
-                     "sampling",
-                     "errortype",
-                     "year"))
-
-eval.df.sep$scale <- as.factor(eval.df.sep$scale)
-levels(eval.df.sep$scale) <- c("statewide",
-                               "large",
-                               "medium",
-                               "small")
-
-aucsameyear <- lm(errornum ~ scale, data = eval.df.sep[eval.df.sep$errortype=="auc"&eval.df.sep$year=="sameyear",])
-Anova(aucsameyear, type = 2)
-plot(errornum ~ scale, data = eval.df.sep[eval.df.sep$errortype=="auc"&eval.df.sep$year=="sameyear",])
-summary(aucsameyear)
-
-aucdiffear <- lm(errornum ~ scale, data = eval.df.sep[eval.df.sep$errortype=="auc"&eval.df.sep$year=="diffyear",])
-Anova(aucdiffear, type = 2)
-plot(errornum ~ scale, data = eval.df.sep[eval.df.sep$errortype=="auc"&eval.df.sep$year=="diffyear",])
-summary(aucdiffear)
-
-
-rmsesameyear <- lm(errornum ~ scale, data = eval.df.sep[eval.df.sep$errortype=="rmse"&eval.df.sep$year=="sameyear",])
-Anova(rmsesameyear, type = 2)
-plot(errornum ~ scale, data = eval.df.sep[eval.df.sep$errortype=="rmse"&eval.df.sep$year=="sameyear",])
-summary(rmsesameyear)
-
-rmsediffear <- lm(errornum ~ scale, data = eval.df.sep[eval.df.sep$errortype=="rmse"&eval.df.sep$year=="diffyear",])
-Anova(rmsediffear, type = 2)
-plot(errornum ~ scale, data = eval.df.sep[eval.df.sep$errortype=="rmse"&eval.df.sep$year=="diffyear",])
-summary(rmsediffear)
+listofeval <- lapply (specieslist,
+                      FUN = eval.function)
+evalsforbinding <- do.call(rbind,
+                           lapply(listofeval, 
+                                  data.frame, 
+                                  stringsAsFactors=FALSE))
 
 
+anova.for.species.error <- function (SPECIES,
+                                     errortypehere,
+                                     yearhere) {
+errormodel <- lm(errornum ~ scale, data = evalsforbinding[evalsforbinding$errortype==errortypehere&
+                                                            evalsforbinding$year==yearhere&
+                                                            evalsforbinding$species==SPECIES,])
+Anova(errormodel, type = 2)
+plot(errornum ~ scale, data = evalsforbinding[evalsforbinding$errortype==errortypehere&
+                                            evalsforbinding$year==yearhere&
+                                              evalsforbinding$species==SPECIES,])
 
-test.of.var <- data.frame("numbioclim" = c(5,8,9,8,2,6,5,3,2,10,2),
-                          "numnonstate" = c(3,2,0,0,1,1,1,0,1,0,3))
-modelvar <- glm(numnonstate ~ numbioclim,
-               data = test.of.var,
-               family = "poisson")
-summary(modelvar)
+
+summary.extracted <- data.frame(summary(errormodel)[[4]])
+
+beta <- summary.extracted$Estimate
+SE <- summary.extracted$Std..Error
+P <- summary.extracted$Pr...t..
+
+return(list(beta,
+            SE,
+            P,
+            summary.extracted,
+            errormodel))
+}
+
+sumanova <- anova.for.species.error("NOBO",
+                        "auc",
+                        "sameyear")
+
+str(sumanova)
+
+
+
+
+
+#Then combine elements 1, 2, 3 into a data frame
+#get adjusted p-value column
+#paste
+#spread
+
+p.adjust(sumanova[[1]]$Pr...t..,
+         method = "holm")
