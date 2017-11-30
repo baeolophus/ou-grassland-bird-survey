22048a6e6f8378e467409c133849974cb4b1bf98
baeolophus <curryclairem@gmail.com>
2017-11-30 14:49:23 -0600

    Edit comments, replace function with source function file.

--- manuscript_files/support_set_pixel_coverage_tests.R
+++ manuscript_files/support_set_pixel_coverage_tests.R
@@ -26,67 +26,8 @@ state<-spTransform(x = state,
 
 set.seed(6798257) #set seed for random points, and later the other random processes.
 
-random.stratified.support.sets <- function (numberofpoints,
-                                            radius){ #radius = 1/2 of a square side
-  random.points<-spsample(x=state, #should be able to use the spatial polygon here too.  or studyarea.extent.poly
-                          n=numberofpoints,
-                          type="stratified")
-  
-  #confirm projection
-  proj4string(random.points)
-  
-  plot(random.points) #view
-  
-  #create squares around them, these will be support set extents.
-  #How to make squares/rectangles, code/comments adapted from here:
-  #http://neondataskills.org/working-with-field-data/Field-Data-Polygons-From-Centroids
-  #they got much from: http://stackoverflow.com/questions/26620373/spatialpolygons-creating-a-set-of-polygons-in-r-from-coordinates
-  #get the centroids from the random.points spatial points object.
-  
-  centroids<-data.frame(
-    "x"=coordinates(random.points)[,1],
-    "y"=coordinates(random.points)[,2]
-  )
-  centroids$ID<-paste("ID", rownames(centroids), sep="")
-  
-  #define the plot boundaries based upon the plot radius. 
-  #NOTE: this assumes that plots are oriented North and are not rotated. 
-  #If the plots are rotated, you'd need to do additional math to find 
-  #the corners.
-  yPlus <- centroids$y+radius
-  xPlus <- centroids$x+radius
-  yMinus <- centroids$y-radius
-  xMinus <- centroids$x-radius
-  
-  #Extract the plot ID information. NOTE: because we set
-  #stringsAsFactor to false above, we can import the plot 
-  #ID's using the code below. If we didn't do that, our ID's would 
-  #come in as factors by default. 
-  #We'd thus have to use the code ID=as.character(centroids$Plot_ID) 
-  ID<-centroids$ID
-  
-  #calculate polygon coordinates for each plot centroid. 
-  square<-cbind(xMinus,yPlus, xPlus,yPlus, xPlus,yMinus, xMinus,yMinus,xMinus,yPlus,xMinus,yPlus)
-  
-  
-  #create spatial polygons
-  polys <- SpatialPolygons(
-    mapply(function(poly, id) {
-      xy <- matrix(poly, ncol=2, byrow=TRUE)
-      Polygons(list(Polygon(xy)), ID=id)
-    },
-    split(square, row(square)), ID),
-    proj4string=CRS(as.character(
-      "+proj=utm +zone=14 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs")))
-  
-  # Create SpatialPolygonDataFrame -- this step is required to output multiple polygons.
-  polys.df <- SpatialPolygonsDataFrame(polys, data.frame(id=ID, row.names=ID))
-  
-  plot(random.points)
-  plot(polys.df,
-       add=TRUE)
-  return(list(polys, polys.df))
-}
+#Function for random.stratified.support.sets used below.
+source("source_ensemble_function_support_set_generation.R")
 
 radius.small <- 60000 #small radius in meters. =6,000 = 60 km = 120 x 120 km boxes #200 points
 radius.medium <- 100000 #med radius in meters. =100,000 = 100 km = 200 x 200 km boxes #75 points
@@ -110,6 +51,7 @@ polys.large.df <- unlist(polys.large[[2]])
 #http://r-sig-geo.2731867.n2.nabble.com/Efficient-way-to-obtain-gridded-count-of-overlapping-polygons-td6034590.html
 #Checking that the three scales have similar numbers of overlaps
 
+#This file is used to rasterize the polygons at its resolution.  Any of the predictors would have worked.
 nlcd_ok_utm14 <- raster("E:/Documents/college/OU-postdoc/research/grassland_bird_surveys/ougrassland/gis_layers_processed/nlcd_processing/nlcd_cropped_to_ok_census/nlcd_ok_utm14_okmask.tif")
 
 beginCluster()
@@ -173,11 +115,15 @@ colnames(overlaps) <- c("overlapsperpixel", "set")
 overlaps$set <- as.factor(overlaps$set)
 endCluster()
 
+#Test for differences.
+
 overlaps.lm <- lm (overlapsperpixel ~ set,
                    data = overlaps)
 anova(overlaps.lm)
 plot(overlapsperpixel ~ set,
      data = overlaps)
+
+
 library(multcomp)
 anova.tukey<-glht(overlaps.lm,
                   linfct=mcp(set="Tukey"))
