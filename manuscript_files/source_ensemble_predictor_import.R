f85cac1f2380971014623007eeb72a41326d8b6d
baeolophus <curryclairem@gmail.com>
2017-11-30 10:58:44 -0600

    Editing and amending comments.

--- manuscript_files/source_ensemble_predictor_import.R
+++ manuscript_files/source_ensemble_predictor_import.R
@@ -14,21 +14,22 @@ wdrasters_files <- paste0(gispath,
 
 for(i in wdrasters_files) { assign(unlist(strsplit(i,
                                                      "[./]"))[5],
-#splits filenames at / and and . to eliminate folder name and file type.
+#Splits filenames at / and and . to eliminate folder name and file type.
 #The number on the previous line must be edited to match the depth of your filesystem so that the 
 #rest of the file creates individual layers that match each raster and then stacks them.
                                      raster(i)) } 
 
 
 #######
-#general turn them into predictors stack.
+#Get all raster layers in the working environment into one object.
 predictors <- as.list(ls()[sapply(ls(), function(x) class(get(x))) == 'RasterLayer'])
-
 predictors.list <- as.list(lapply(predictors, get))
 #using get lets the middle code take the character names of raster layers and stack everything that is a raster layer
-#Using lapply on the list lets it do this to all the rasters.
+#using lapply on the list lets it do this to all the rasters.
 
 
+#stack the rasters.
 predictors_stack <- stack (predictors.list)
+
 #Define study area extent based on predictors.
 (studyarea.extent <- extent(predictors_stack))
