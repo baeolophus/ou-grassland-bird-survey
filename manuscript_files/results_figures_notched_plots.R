cff281f764939e3df98a13ccd408fcfbccf0e5bc
baeolophus <curryclairem@gmail.com>
2018-06-27 13:33:40 -0500

    creation of two figures (5 and 6) showing error for each species and error type and all data resolutions.

--- manuscript_files/results_figures_notched_plots.R
+++ manuscript_files/results_figures_notched_plots.R
@@ -16,16 +16,62 @@ errors <- rbind(auc_current,
                 rmse_downscale)
 
 library(ggplot2)
-
-ggplot()+
-  geom_boxplot(data = errors,
+library(lemon)
+svg(file = "manuscript_files/Figure5.svg",
+    width = 8.5,
+    height = 11)
+fig5 <- ggplot()+
+  geom_boxplot(data = subset(errors, errortype == "rmse"),
                aes(x = scale,
                    y = evalue,
                    fill = predictor_res),
                notch = TRUE)+
-  facet_wrap(errortype~species,
+  facet_wrap(~species,
              scales = "free",
-             nrow = 2,
-             ncol = 11
+             nrow = 4,
+             ncol = 3
              )+
-  theme_bw()
+  theme_bw()+
+  scale_y_continuous(minor_breaks = seq(0, 1, 0.0025))+
+  scale_fill_manual(values=c("white", "darkgray"),
+                    name = "Predictor resolution")+
+  labs( x = "Model scale",
+          y = "RMSE")
+
+#Get panel name for lower rightmost panel.
+gtable_show_names(fig5)
+#put legend in that empty facet panel.
+reposition_legend(fig5,
+                  position = "center",
+                  panel = "panel-3-4")
+dev.off()
+
+#AUC
+svg(file = "manuscript_files/Figure6.svg",
+    width = 8.5,
+    height = 11)
+fig6 <- ggplot()+
+  geom_boxplot(data = subset(errors, errortype == "auc"),
+               aes(x = scale,
+                   y = evalue,
+                   fill = predictor_res),
+               notch = TRUE)+
+  facet_wrap(~species,
+             scales = "free",
+             nrow = 4,
+             ncol = 3
+  )+
+  theme_bw()+
+  scale_y_continuous(minor_breaks = seq(0, 1, 0.0025))+
+  scale_fill_manual(values=c("white", "darkgray"),
+                    name = "Predictor resolution")+
+  labs( x = "Model scale",
+        y = "AUC")
+
+#Get panel name for lower rightmost panel.
+gtable_show_names(fig6)
+#put legend in that empty facet panel.
+reposition_legend(fig6,
+                  position = "center",
+                  panel = "panel-3-4")
+dev.off()
